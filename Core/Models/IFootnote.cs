6636f55bb74cd5636edac046b5cfdfb10c05d689
Graham Scanlon <git@ascallion.com>
2026-02-13 12:29:26 -0600

    wip: shared footnotes

---
+++ Core/Models/IFootnote.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Core.Models;
+
+
+public interface IFootnote
+{
+    int Id { get; init; }
+
+    /// <summary>
+    /// A helpful snippet of fitness advice to show the users.
+    /// </summary>
+    [Required]
+    string Note { get; init; }
+
+    /// <summary>
+    /// Either a link or a name that was the reference of the note.
+    /// </summary>
+    string? Source { get; init; }
+
+    /// <summary>
+    /// Affirmations vs Fitness Tips.
+    /// </summary>
+    FootnoteType Type { get; init; }
+}
---
+++ Data/ComContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Data.Entities.Footnote;
+
+namespace Shared.Data;
+
+/// <summary>
+/// https://mehdi.me/ambient-dbcontext-in-ef6/
+/// </summary>
+public class ComContext : DbContext
+{
+    [Obsolete("Public parameterless constructor required for EF Core.", error: true)]
+    public ComContext() : base() { }
+    public ComContext(DbContextOptions<ComContext> context) : base(context) { }
+
+    public DbSet<Footnote> Footnotes { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
