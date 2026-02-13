5c9e87856cc6f584182270aa2ce7dc5da8930658
Graham Scanlon <git@ascallion.com>
2026-02-13 15:38:59 -0600

    wip: shared footnotes

--- Core/Models/Footnote/FootnoteType.cs
+++ Core/Models/Footnote/FootnoteType.cs
@@ -2,7 +2,6 @@
 
 namespace ADay.Core.Models.Footnote;
 
-
 [Flags]
 public enum FootnoteType
 {
@@ -10,56 +9,10 @@ public enum FootnoteType
     None = 0,
 
     /// <summary>
-    /// Fitness advice and tips. 
-    /// 
-    /// sa. Take five to 10 minutes to warm up and cool down properly.
-    /// </summary>
-    [Display(Name = "Fitness Tips")]
-    FitnessTips = 1 << 0, // 1
-
-    /// <summary>
-    /// Life advice and tips.
-    /// 
-    /// sa. Practicing everyday mindfulness can improve your memory and concentration skills....
-    /// </summary>
-    [Display(Name = "Health Tips")]
-    HealthTips = 1 << 1, // 2
-
-    /// <summary>
     /// User defined footnotes.
     /// </summary>
-    [Display(Name = "Fitness Facts")]
-    FitnessFacts = 1 << 2, // 4
-
-    /// <summary>
-    /// User defined footnotes.
-    /// </summary>
-    [Display(Name = "Health Facts")]
-    HealthFacts = 1 << 3, // 8
-
-    /// <summary>
-    /// Fitness motivation.
-    /// 
-    /// sa. Never give up!
-    /// </summary>
-    [Display(Name = "Fitness Motivation")]
-    FitnessMotivation = 1 << 4, // 16
-
-    /// <summary>
-    /// Fitness affirmations. 
-    /// 
-    /// sa. I'm getting stronger after every workout
-    /// </summary>
-    [Display(Name = "Fitness Affirmations")]
-    FitnessAffirmations = 1 << 5, // 32
-
-    /// <summary>
-    /// Mindfulness
-    /// 
-    /// sa. Breathe deeply. You are in the present moment.
-    /// </summary>
-    [Display(Name = "Mindfulness")]
-    Mindfulness = 1 << 6, // 64
+    [Display(Name = "Custom")]
+    Custom = 1 << 0, // 1
 
     /// <summary>
     /// Good vibes. Re-parenting.
@@ -67,15 +20,95 @@ public enum FootnoteType
     /// sa. You are beautiful!
     /// </summary>
     [Display(Name = "Good Vibes")]
-    GoodVibes = 1 << 7, // 128
+    GoodVibes = 1 << 1, // 2
+
+    /// <summary>
+    /// Mindfulness
+    /// 
+    /// sa. Breathe deeply. You are in the present moment.
+    /// </summary>
+    [Display(Name = "Mindfulness")]
+    Mindfulness = 1 << 2, // 4
+
+    /// <summary>
+    /// Life advice and tips.
+    /// 
+    /// sa. Practicing everyday mindfulness can improve your memory and concentration skills....
+    /// </summary>
+    [Display(Name = "Health Tips")]
+    HealthTips = 1 << 3, // 8
+
+    /// <summary>
+    /// Health facts.
+    /// </summary>
+    [Display(Name = "Health Facts")]
+    HealthFacts = 1 << 4, // 16
+
+    /// <summary>
+    /// Fitness advice and tips. 
+    /// 
+    /// sa. Take five to 10 minutes to warm up and cool down properly.
+    /// </summary>
+    [Display(Name = "Fitness Tips")]
+    FitnessTips = 1 << 5, // 32
 
     /// <summary>
     /// User defined footnotes.
     /// </summary>
-    [Display(Name = "Custom")]
-    Custom = 1 << 8, // 256
+    [Display(Name = "Fitness Facts")]
+    FitnessFacts = 1 << 6, // 64
 
-    System = All & ~Custom, // FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations | HealthTips | HealthFacts | GoodVibes | Mindfulness
+    /// <summary>
+    /// Fitness motivation.
+    /// 
+    /// sa. Never give up!
+    /// </summary>
+    [Display(Name = "Fitness Motivation")]
+    FitnessMotivation = 1 << 7, // 128
 
-    All = FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations | HealthTips | HealthFacts | GoodVibes | Mindfulness | Custom
+    /// <summary>
+    /// Fitness affirmations. 
+    /// 
+    /// sa. I'm getting stronger after every workout
+    /// </summary>
+    [Display(Name = "Fitness Affirmations")]
+    FitnessAffirmations = 1 << 8, // 256
+
+    /// <summary>
+    /// Fitness advice and tips. 
+    /// 
+    /// sa. Take five to 10 minutes to warm up and cool down properly.
+    /// </summary>
+    [Display(Name = "Cooking Tips")]
+    CookingTips = 1 << 9, // 512
+
+    /// <summary>
+    /// Life advice and tips.
+    /// 
+    /// sa. Practicing everyday mindfulness can improve your memory and concentration skills....
+    /// </summary>
+    [Display(Name = "Ingredient Tips")]
+    IngredientTips = 1 << 10, // 1024
+
+    /// <summary>
+    /// Life motivation.
+    /// 
+    /// sa. Never give up!
+    /// </summary>
+    [Display(Name = "Cooking Motivation")]
+    CookingMotivation = 1 << 11, // 2048
+
+    /// <summary>
+    /// Life affirmations. 
+    /// 
+    /// sa. I'm a thoughtful and interesting person.
+    /// </summary>
+    [Display(Name = "Cooking Affirmations")]
+    CookingAffirmations = 1 << 12, // 4096
+
+    System = All & ~Custom, // Exerything except Custom.
+
+    All = HealthTips | HealthFacts | GoodVibes | Mindfulness | Custom
+        | FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations
+        | CookingAffirmations | CookingMotivation | CookingTips | IngredientTips
 }
